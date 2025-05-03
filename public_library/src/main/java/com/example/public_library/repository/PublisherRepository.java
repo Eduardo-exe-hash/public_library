2f8552cb8b0e1bc64c785f4677088735b4f21e3e
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2025-05-03 12:37:53 -0300

    Update Publisher Repository

--- public_library/src/main/java/com/example/public_library/repository/PublisherRepository.java
+++ public_library/src/main/java/com/example/public_library/repository/PublisherRepository.java
@@ -1,6 +1,13 @@
 package com.example.public_library.repository;
 
 import com.example.public_library.model.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+    List<Publisher> findByNameContainingIgnoreCase(String name);
+}
