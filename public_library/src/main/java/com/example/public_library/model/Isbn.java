931b9364d62f46d9dde4234170c7bef5fbc32558
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2025-05-03 12:35:24 -0300

    Update Isbn

--- public_library/src/main/java/com/example/public_library/model/Isbn.java
+++ public_library/src/main/java/com/example/public_library/model/Isbn.java
@@ -5,54 +5,69 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "db_isbn")
 public class Isbn {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String number;
 
     private String edition;
 
     private Integer yearPublication;
 
-  public User(){}
-  
-  public User(Long id, String number, String edition, String yearPublication) {
-      this.id = id;
-      this.number = number;
-      this.eddition = edition;
-      this.yearPublication = yearPublication;
+    @ManyToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
+
+    public Isbn() {}
+
+    public Isbn(Long id, String number, String edition, Integer yearPublication, Publisher publisher) {
+        this.id = id;
+        this.number = number;
+        this.edition = edition;
+        this.yearPublication = yearPublication;
+        this.publisher = publisher;
     }
 
-  public Long getId() {
-      return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-      this.id = id;
-  }
+    public String getNumber() {
+        return number;
+    }
 
-  public String getNumber() {
-      return number;
-  }
+    public String getEdition() {
+        return edition;
+    }
 
-  public void setNumber(String number) {
-      this.number = number;
-  }
+    public Integer getYearPublication() {
+        return yearPublication;
+    }
 
-  public String getEdition() {
-      return edition;
-  }
+    public Publisher getPublisher() {
+        return publisher;
+    }
 
-  public void setEdition(String edition) {
-      this.edition = edition;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getYearPublication() {
-      return yearPublication;
-  }
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
-  public void setYearPublication(String yearPublication) {
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public void setYearPublication(Integer yearPublication) {
         this.yearPublication = yearPublication;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 }
