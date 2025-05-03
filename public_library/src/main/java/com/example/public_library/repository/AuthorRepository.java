8ec1d5795a48d4444bb3a7da00b452af94e624c0
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2025-05-03 12:37:01 -0300

    Update Author Repository

--- public_library/src/main/java/com/example/public_library/repository/AuthorRepository.java
+++ public_library/src/main/java/com/example/public_library/repository/AuthorRepository.java
@@ -1,6 +1,13 @@
 package com.example.public_library.repository;
 
 import com.example.public_library.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<Author> findByNameContainingIgnoreCase(String name);
+}
